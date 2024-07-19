66354585f8b82d628f818e406723894a63d3c532
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-07-19 16:19:31 +0300

    add second test

--- conftest.py
+++ conftest.py
@@ -2,10 +2,11 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def window_size():
     browser.config.window_height = 915
     browser.config.window_width = 412
+    browser.open('https://google.com')
 
     yield
 
--- test_google_search.py
+++ test_google_search.py
@@ -1,19 +1,11 @@
-import pytest
 from selene import browser, be, have
 
 
-# @pytest.fixture()
-# def window_size():
-#     browser.config.window_height = 915
-#     browser.config.window_width = 412
-#
-#     yield
-#
-#     browser.quit()
-
-
 def test_first(window_size):
-    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
+
+def test_second(window_size):
+    browser.element('[name="q"]').should(be.blank).type('kjdfngoi47u20').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('По запросу kjdfngoi47u20 ничего не найдено. '))
